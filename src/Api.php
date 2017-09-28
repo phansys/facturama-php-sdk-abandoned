1cfd0c44e2c50115140f5e8af4d43647c5917ede
Javier Spagnoletti <phansys@gmail.com>
2017-09-28 11:33:25 -0300

    Replaced raw curl HTTP client implementation with `guzzlehttp/guzzle` at `Api`

--- src/Api.php
+++ src/Api.php
@@ -11,6 +11,12 @@
 
 namespace Facturama;
 
+use Facturama\Exception\ModelException;
+use Facturama\Exception\RequestException;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException as GuzzleRequestException;
+
 /**
  * Facturama API
  *
@@ -27,42 +33,34 @@ class Api
      */
     const API_URL = 'https://www.api.facturama.com.mx/api';
     /**
-     * Configuration for CURL
+     * @var string
      */
-    protected $curl_opts = [
-        CURLOPT_USERAGENT => 'FACTURAMA-PHP-SDK-1.0.0',
-        //CURLOPT_SSL_VERIFYPEER => true,
-        CURLOPT_CONNECTTIMEOUT => 10,
-        CURLOPT_HEADER => true,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 60,
-    ];
+    private $user;
     /**
-     * User Name
-     *
-     * @var null
+     * @var string
      */
-    protected $user = null;
+    private $password;
     /**
-     * Password
-     *
-     * @var null
+     * @var ClientInterface
      */
-    protected $password = null;
+    private $client;
 
     /**
      * Init configuration
      *
      * @param string $user     username
      * @param string $password password
-     * @param mixed $curl_opts curl options
+     * @param array $config
      */
-    public function __construct($user = null, $password = null, $curl_opts = null)
+    public function __construct($user = null, $password = null, array $config = [])
     {
         $this->user = $user ? $user : config('facturama.credentials.username');
         $this->password = $password ? $password : config('facturama.credentials.password');
-
-        $this->curl_opts = $curl_opts ? array_merge($this->curl_opts, $curl_opts) : $this->curl_opts;
+        $this->client = new Client($config + ['headers' => ['User-Agent' => 'FACTURAMA-PHP-SDK-1.0.0']]);
+        $this->client->setDefaultOption('verify', false);
+        $this->client->setDefaultOption('auth', [$user, $password]);
+        $this->client->setDefaultOption('connect_timeout', 10);
+        $this->client->setDefaultOption('timeout', 60);
     }
 
     /**
@@ -71,164 +69,96 @@ class Api
      * @param  string $path
      * @param  array $params
      *
-     * @return mixed
+     * @return \stdClass
      */
-    public function get($path, $params = null)
+    public function get($path, array $params = [])
     {
-        $opts = [
-            CURLOPT_HTTPHEADER => [
-                'Content-Type: application/json',
-                'Authorization: Basic '.$this->getCredentials(),
-            ],
-        ];
-        $exec = $this->execute($path, null, $params);
-
-        return $exec;
+        return $this->executeRequest('GET', $path, ['query' => $params]);
     }
 
     /**
      * POST Request
      *
      * @param  string $path
-     * @param  array $body
-     * @param  array  $params
+     * @param  array|null $body
+     * @param  array $params
      *
-     * @return mixed
+     * @return \stdClass
      */
-    public function post($path, $body = null, $params = [])
+    public function post($path, array $body = null, array $params = [])
     {
-        $body = json_encode($body);
-        $opts = [
-            CURLOPT_HTTPHEADER => [
-                'Content-Type: application/json',
-                'Authorization: Basic '.$this->getCredentials(),
-            ],
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $body,
-        ];
-        $exec = $this->execute($path, $opts, $params);
-
-        return $exec;
+        return $this->executeRequest('POST', $path, ['json' => $body, 'query' => $params]);
     }
 
     /**
      * PUT Request
      *
      * @param  string $path
-     * @param  mixed $body
+     * @param  array|null $body
      * @param  array $params
      *
-     * @return mixed
+     * @return \stdClass
      */
-    public function put($path, $body = null, $params = null)
+    public function put($path, array $body = null, array $params = [])
     {
-        $body = json_encode($body);
-        $opts = [
-            CURLOPT_HTTPHEADER => [
-                'Content-Type: application/json',
-                'Authorization: Basic '.$this->getCredentials(),
-            ],
-            CURLOPT_CUSTOMREQUEST => 'PUT',
-            CURLOPT_POSTFIELDS => $body,
-        ];
-
-        $exec = $this->execute($path, $opts, $params);
-
-        return $exec;
+        return $this->executeRequest('PUT', $path, ['json' => $body, 'query' => $params]);
     }
 
     /**
      * DELETE Request
      *
      * @param  string $path
-     * @param  mixed $params
+     * @param  array $params
      *
-     * @return mixed
+     * @return \stdClass
      */
-    public function delete($path, $params = null)
+    public function delete($path, array $params = [])
     {
-        $opts = [
-            CURLOPT_HTTPHEADER => [
-                'Content-Type: application/json',
-                'Authorization: Basic '.$this->getCredentials(),
-            ],
-            CURLOPT_CUSTOMREQUEST => 'DELETE',
-        ];
-
-        $exec = $this->execute($path, $opts, $params);
-
-        return $exec;
+        return $this->executeRequest('DELETE', $path, ['query' => $params]);
     }
 
     /**
-     * Execute all requests and returns the json body and headers
+     * Execute the request and return the resulting object
      *
-     * @param  string $path
-     * @param  array  $opts
-     * @param  array  $params
+     * @param string $method
+     * @param string $url
+     * @param array $options
      *
-     * @return mixed
+     * @throws \RuntimeException|\LogicException
+     *
+     * @return \stdClass
      */
-    public function execute($path, $opts = [], $params = [])
+    private function executeRequest($method, $url, array $options = [])
     {
-        $uri = $this->make_path($path, $params);
-        $ch = curl_init($uri);
+        try {
+            $request = $this->client->createRequest($method, sprintf('%s/%s', self::API_URL, $url), $options);
+            $response = $this->client->send($request);
+            $content = trim($response->getBody()->getContents());
+        } catch (GuzzleRequestException $e) {
+            if ($e->hasResponse()) {
+                $content = trim($e->getResponse()->getBody()->getContents());
+                if (($object = json_decode($content)) && isset($object->Message)) {
+                    $modelException = null;
+                    if (isset($object->ModelState)) {
+                        $modelExceptionMessages = [];
+                        foreach ($object->ModelState as $invalidPropertyMessages) {
+                            $modelExceptionMessages = array_merge($modelExceptionMessages, $invalidPropertyMessages);
+                        }
+                        $modelExceptionMessage = null;
+                        if ($modelExceptionMessages) {
+                            $modelExceptionMessage = implode('; ', $modelExceptionMessages).'.';
+                        }
+                        $modelException = new ModelException($modelExceptionMessage, $e->getCode());
+                    }
+                    throw new RequestException($object->Message, 0, $modelException);
+                }
 
-        curl_setopt_array($ch, $this->curl_opts);
-        curl_setopt($ch, CURLOPT_USERPWD, $this->user.':'.$this->password);
-        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-        curl_getinfo($ch, CURLINFO_HEADER_OUT);
-
-        if (!empty($opts)) {
-            curl_setopt_array($ch, $opts);
-        }
-
-        $response = curl_exec($ch);
-        $response = preg_split('/^\r?$/m', $response, 2);
-        $return['body'] = json_decode(trim($response[1]));
-        $return['httpCode'] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        return $return;
-    }
-
-    /**
-     * Check and construct an real URL to make request
-     *
-     * @param  string $path
-     * @param  array  $params
-     *
-     * @return string
-     */
-    public function make_path($path, $params = [])
-    {
-        if (!preg_match('/^http/', $path)) {
-            if (!preg_match("/^\//", $path)) {
-                $path = '/'.$path;
+                throw new RequestException($content ?: $e->getMessage(), $e->getCode());
             }
-            $uri = self::API_URL.$path;
-        } else {
-            $uri = $path;
-        }
-        if (!empty($params)) {
-            $paramsJoined = [];
-            foreach ($params as $param => $value) {
-                $paramsJoined[] = "$param=$value";
-            }
-            $params = '?'.implode('&', $paramsJoined);
-            $uri = $uri.$params;
+
+            throw new RequestException($e->getMessage(), $e->getCode());
         }
 
-        return $uri;
-    }
-
-    /**
-     * Generate Basic Auth
-     *
-     * @return string
-     */
-    protected function getCredentials()
-    {
-        return base64_encode($this->user.':'.$this->password);
+        return json_decode($content);
     }
 }
---
+++ src/Exception/ModelException.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of Facturama PHP SDK.
+ *
+ * (c) Javier Telio <jtelio118@gmail.com>
+ *
+ * This source file is subject to a MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Facturama\Exception;
+
+/**
+ * @author Javier Spagnoletti <phansys@gmail.com>
+ */
+class ModelException extends \LogicException
+{
+}
